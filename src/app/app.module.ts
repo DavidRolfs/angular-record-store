8f01f2922ce02d981ab9171ad5647efc3d2397da
Hunter Parks and Jordan Mysliwiec <hunter+jordan@>
2017-07-10 16:20:47 -0700

    add completed homework and extended monday lessons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { Album } from './album.model';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     AboutComponent,
-    MarketplaceComponent
+    MarketplaceComponent,
+    AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
